c4bedf21ec2fca927997fdee9f142a1013f30734
Steven Huelsmeier <nakrul@web.de>
2021-12-22 12:30:27 +0100

    Add CHANGELOG.md verification

---
+++ KeepAChangelogParser.Tests/Services/FileService.cs
@@ -0,0 +1,103 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.IO;
+using System.Security;
+
+namespace KeepAChangelogParser.Tests.Services
+{
+
+  public static class FileService
+  {
+
+    public static Result<string> ReadText(
+      string filePath
+    )
+    {
+      if (!File.Exists(filePath))
+      {
+        return Result.Failure<string>(
+          $"The file {filePath} does not exist!");
+      }
+
+      FileStream? fileStream = null;
+
+      try
+      {
+        try
+        {
+          fileStream =
+            new FileStream(
+              filePath,
+              FileMode.Open,
+              FileAccess.Read);
+        }
+        catch (Exception exception)
+        when (exception is ArgumentNullException
+                        or ArgumentException
+                        or NotSupportedException
+                        or FileNotFoundException
+                        or IOException
+                        or SecurityException
+                        or DirectoryNotFoundException
+                        or UnauthorizedAccessException
+                        or PathTooLongException
+                        or ArgumentOutOfRangeException)
+        {
+          return Result.Failure<string>(exception.Message);
+        }
+
+        StreamReader? streamReader = null;
+
+        try
+        {
+          try
+          {
+            streamReader =
+              new StreamReader(
+                fileStream);
+          }
+          catch (Exception exception)
+          when (exception is ArgumentException
+                          or ArgumentNullException)
+          {
+            return Result.Failure<string>(exception.Message);
+          }
+
+          string text = string.Empty;
+
+          try
+          {
+            text =
+              streamReader.ReadToEnd();
+
+            return Result.Success(text);
+          }
+          catch (Exception exception)
+          when (exception is OutOfMemoryException
+                          or IOException)
+          {
+            return Result.Failure<string>(exception.Message);
+          }
+        }
+        finally
+        {
+          if (streamReader != null)
+          {
+            streamReader.Close();
+            streamReader.Dispose();
+          }
+        }
+      }
+      finally
+      {
+        if (fileStream != null)
+        {
+          fileStream.Close();
+          fileStream.Dispose();
+        }
+      }
+    }
+
+  }
+
+}
---
+++ KeepAChangelogParser.Tests/TheChangelogIsValid.cs
@@ -0,0 +1,56 @@
+﻿using CSharpFunctionalExtensions;
+using KeepAChangelogParser.Models;
+using KeepAChangelogParser.Tests.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace KeepAChangelogParser.Tests
+{
+
+  [TestClass]
+  public class TheChangelogIsValid
+  {
+
+    [TestMethod]
+    public void WhenTheChangelogIsParsed_ThenTheResultIsCorrect()
+    {
+      // Arrange
+      string textToParse =
+        createTextToParse();
+
+      IChangelogParser changelogParser =
+        new ChangelogParser();
+
+      // Act
+      Result<Changelog> actualParseResult =
+        changelogParser.Parse(
+          textToParse);
+
+      // Assert
+      Assert.IsTrue(
+        actualParseResult.IsSuccess,
+        actualParseResult.IsSuccess ? string.Empty : actualParseResult.Error);
+    }
+
+    private static string createTextToParse()
+    {
+      string changeLogPath;
+      string currentDirectory = Environment.CurrentDirectory;
+
+      while (true)
+      {
+        changeLogPath = Path.Combine(currentDirectory, "CHANGELOG.md");
+        if (File.Exists(changeLogPath)) { break; }
+
+        DirectoryInfo? directoryInfo = Directory.GetParent(currentDirectory);
+        if (directoryInfo == null) { break; }
+
+        currentDirectory = directoryInfo.FullName;
+      }
+
+      return FileService.ReadText(changeLogPath).Value;
+    }
+  }
+
+}
