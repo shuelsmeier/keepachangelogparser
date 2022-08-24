53e3f0bb6b5a9f64abf3a48ac11ac3d5b03852b3
Steven Huelsmeier <nakrul@web.de>
2022-08-24 20:30:36 +0200

    Fix IDE0008 warning

--- KeepAChangelogParser.Wpf.SampleApp/ViewModels/BaseViewModel.cs
+++ KeepAChangelogParser.Wpf.SampleApp/ViewModels/BaseViewModel.cs
@@ -48,7 +48,7 @@ namespace KeepAChangelogParser.Wpf.SampleApp.ViewModels
       PropertyChangedEventHandler? handler = this.PropertyChanged;
       if (handler != null)
       {
-        var e = new PropertyChangedEventArgs(propertyName);
+        PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
         handler(this, e);
       }
     }
