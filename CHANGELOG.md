e4d7e4725523eeaac8db19a180c6fa03ed7af2cf
Steven Huelsmeier <nakrul@web.de>
2025-10-11 23:41:05 +0200

    Bump MSTest.TestAdapter and MSTest.TestFramework from 3.8.3 to 4.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Bump Microsoft.NET.Test.Sdk from 17.13.0 to 18.0.0
+- Bump MSTest.TestAdapter from 3.8.3 to 4.0.0
+- Bump MSTest.TestFramework from 3.8.3 to 4.0.0
 
 
 ## [1.2.8] - 2025-10-11
--- KeepAChangelogParser.Tests/TheChangelogContainsMixedCRAndCRLFLineEndings.cs
+++ KeepAChangelogParser.Tests/TheChangelogContainsMixedCRAndCRLFLineEndings.cs
@@ -24,8 +24,8 @@ namespace KeepAChangelogParser.Tests
 
       // Assert
       Assert.AreEqual(
-        actualParseResult.Error,
-        "Text contains mixed line endings CR and CRLF.");
+        "Text contains mixed line endings CR and CRLF.",
+        actualParseResult.Error);
     }
 
     private static string createTextToParse()
